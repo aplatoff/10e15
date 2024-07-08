b9155d9f44b76fbf6719eb2887980ec2108d9d53
Andrey Platov <andrey@hardcoreeng.com>
2024-07-08 04:11:55 +0200

    implement reconnect
    Signed-off-by: Andrey Platov <andrey@hardcoreeng.com>

--- packages/proto/src/client.ts
+++ packages/proto/src/client.ts
@@ -4,23 +4,38 @@ export const createClient = (url: string) => {
   const queue: ArrayBufferLike[] = []
   const listeners: ((buf: ArrayBuffer) => void)[] = []
 
-  const ws = new WebSocket(url)
-  ws.onopen = processQueue
-  ws.onclose = () => {
-    console.log('connection closed')
+  let ws: WebSocket | null = null
+
+  function initWebSocket() {
+    ws = new WebSocket(url)
+    ws.onopen = processQueue
+    ws.onclose = () => {
+      console.log('connection closed')
+      scheduleReconnect()
+    }
+    ws.onerror = (e) => {
+      console.error('connection error', e)
+      scheduleReconnect()
+    }
+    ws.onmessage = (event) => {
+      const blob = event.data as Blob
+      blob.arrayBuffer().then((buffer) => {
+        listeners.forEach((listener) => listener(buffer))
+      })
+    }
   }
-  ws.onerror = (e) => {
-    console.error('connection error', e)
-  }
-  ws.onmessage = (event) => {
-    const blob = event.data as Blob
-    blob.arrayBuffer().then((buffer) => {
-      listeners.forEach((listener) => listener(buffer))
-    })
+
+  function scheduleReconnect() {
+    const timeout = 1000
+    console.log(`Attempting to reconnect in ${timeout / 1000} seconds...`)
+    setTimeout(() => {
+      console.log('Reconnecting...')
+      initWebSocket()
+    }, timeout)
   }
 
   function processQueue() {
-    if (ws.readyState === ws.OPEN) {
+    if (ws && ws.readyState === ws.OPEN) {
       while (true) {
         const data = queue.shift()
         if (data === undefined) break
@@ -28,6 +43,8 @@ export const createClient = (url: string) => {
       }
     }
   }
+
+  initWebSocket()
 
   return {
     postRequest(buffer: ArrayBufferLike) {
