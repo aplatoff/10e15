a3c7140d2dad6ee9cbb39add3f20808d8567afff
Andrey Platov <andrey@hardcoreeng.com>
2024-07-08 02:00:57 +0200

    port change
    Signed-off-by: Andrey Platov <andrey@hardcoreeng.com>

--- packages/server/src/pages.ts
+++ packages/server/src/pages.ts
@@ -1,5 +1,5 @@
 //
-import type { PageNo, Time } from 'model'
+import type { PageNo } from 'model'
 import { getPath } from './config'
 
 const cors = {
@@ -14,26 +14,31 @@ function createServer(port: number) {
       if (req.method === 'OPTIONS') {
         return new Response(null, { status: 200, headers: cors })
       }
-      const url = new URL(req.url)
-      const pageId = url.pathname.slice(1)
-      const pageCode = pageId.split('-')
-      const page = Number(pageCode[0]) as PageNo
-      const time = BigInt(pageCode[1]) as Time
-      const path = getPath(page)
-      const file = Bun.file(path)
-      console.log('serving', path, file.size, file.type)
-      return new Response(file, {
-        headers: {
-          ...cors,
-          'Cache-Control': 'public, max-age=31536000',
-          ETag: `"${pageId}"`,
-        },
-      })
+      try {
+        const url = new URL(req.url)
+        const pageId = url.pathname.slice(1)
+        const pageCode = pageId.split('-')
+        const page = Number(pageCode[0]) as PageNo
+        //const time = BigInt(pageCode[1]) as Time
+        const path = getPath(page)
+        const file = Bun.file(path)
+        console.log('serving', path, file.size, file.type)
+        return new Response(file, {
+          headers: {
+            ...cors,
+            'Cache-Control': 'public, max-age=31536000',
+            ETag: `"${pageId}"`,
+          },
+        })
+      } catch (e) {
+        console.error(e)
+        return new Response('Not found', { status: 404 })
+      }
     },
     port,
   })
   return server
 }
 
-const server = createServer(8000)
+const server = createServer(8080)
 console.log(`server running on port ${server.port}`)
